727b2ca572cf7bf689c2ec96904896cb386a3f0c
Min-SungJo <123or@naver.com>
2022-10-29 16:19:31 +0900

    create-ghost

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Header from './components/Header';
+import './style2.css';
 
 function App() {
   return (
     <div>
-      <Headers/>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+const Header = () => {
+
+  return (
+    <div className='ghost'>
+      <div className='body'>
+        <div className='face'>
+          <div className='eye'></div>
+          <div className='eye'></div>
+          <div className='mouse'></div>
+        </div>
+      </div>
+      <div className='bottom'>
+        <div className='leg'></div>
+        <div className='leg'></div>
+        <div className='leg'></div>
+        <div className='leg'></div>
+        <div className='leg'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
